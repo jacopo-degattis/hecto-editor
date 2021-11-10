eb412ce63dbe8ddab0c9b22adacbd7d9c3cb81d5
Jacopo De Gattis <jacopo.degattis@gmail.com>
2021-11-10 10:43:30 +0100

    fix cursor overflow w, h

--- src/editor.rs
+++ src/editor.rs
@@ -121,11 +121,24 @@ impl Editor {
   
   fn move_cursor(&mut self, key: Key) {
     let Position { mut x, mut y } = self.cursor_position;
+    
+    let size = self.terminal.size();
+    // you can go until the width - 1, the last time you cannot go further anymore
+    let height = size.height.saturating_sub(1) as usize;
+    let width = size.width.saturating_sub(1) as usize;
     match key {
       Key::Up => y = y.saturating_sub(1),
-      Key::Down => y = y.saturating_add(1),
+      Key::Down => {
+        if y < height {
+          y = y.saturating_add(1);
+        }
+      },
       Key::Left => x = x.saturating_sub(1),
-      Key::Right => x = x.saturating_add(1),
+      Key::Right => {
+        if x < width {
+          x = x.saturating_add(1);
+        }
+      },
       _ => (),
     }
     self.cursor_position = Position { x, y }
